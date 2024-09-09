b52ef1458d5855cbf1ffb240488f189cb67f3aa1
aryal05 <aryal.rajat05@gmail.com>
2024-09-09 19:51:36 +0545

    App .jsx calculator logics

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import style from "./App.module.css"
+import Display from "./components/display"
+import ButtonsContainer from "./components/ButtonsContainer"
+import { useState } from "react"
 
-const App = () => {
-  return (
-    <div>
-      Clculator
+function App() {
+  const [calVal, setCalVal] = useState("")
+
+  const onButtonClick= (buttonText)=>{
+    if (buttonText === "C"){
+      setCalVal("");
+
+    }else if (buttonText === "="){
+      const result = eval(calVal);
+      setCalVal(result)
+
       
+
+    }else{
+      const newDisplayVal = calVal + buttonText;
+      setCalVal(newDisplayVal)
+
+    }
+  }
+
+  return (
+    <div className= {style.calculator}>
+     <Display displayVal= {calVal}></Display>
+     <ButtonsContainer onButtonClick = {onButtonClick}></ButtonsContainer>
+  
     </div>
   )
 }
