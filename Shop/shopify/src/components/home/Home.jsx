1bc942ddcafd060168f117e4989ece79d0029683
aryal05 <aryal.rajat05@gmail.com>
2024-10-17 17:03:02 +0545

    home file setup

--- Shop/shopify/src/components/home/Home.jsx
+++ Shop/shopify/src/components/home/Home.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      hwllo
+
+    <div className='flex items-center justify-around'>
+        <div>
+            <h1>Home section components
+
+            </h1>
+        </div>
+
+        <div>
+            <h1>Home section components
+
+            </h1>
+        </div>
+        
+        <div>
+            <h1>Home section components
+
+            </h1>
+        </div>
+
     </div>
+    
   )
 }
 
