08981aaec2137bbabb24c0ed87e202980308757d
aryal05 <aryal.rajat05@gmail.com>
2024-10-17 16:22:30 +0545

    file setup for navbar

--- Shop/shopify/src/App.jsx
+++ Shop/shopify/src/App.jsx
@@ -1,10 +1,16 @@
+import NavBar from "./components/home/NavBar"
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      Hery
-    </div>
+    <>
+    
+  <NavBar/>
+     <Outlet/>
+  
+     
+    
+    </>
   )
 }
 
--- Shop/shopify/src/main.jsx
+++ Shop/shopify/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/home/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,  // This makes sure the home route '/' renders the Home component
+        element: <Home/>,
+      },
+      // {
+      //   path: 'contact',
+      //   element: <Contact />,
+      // },
+      // {
+      //   path: 'about',
+      //   element: <About />,
+      // },
+      // {
+      //   path: 'shop',
+      //   element: <Shop/>,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
