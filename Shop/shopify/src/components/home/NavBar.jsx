75f351c1ce4367125d56742f13dbb1f91ac662be
aryal05 <aryal.rajat05@gmail.com>
2024-10-17 16:38:23 +0545

    ul section for navbae

--- Shop/shopify/src/components/home/NavBar.jsx
+++ Shop/shopify/src/components/home/NavBar.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-        <h1>Hello this is navbar section</h1>
+
+       <div className='flex justify-around items-center'>
+        <ul className='flex space-x-10 mt-5 '>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Section</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>   
+        </ul>
+       </div>
       
-    </div>
+    
   )
 }
 
