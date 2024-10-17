6fbadb7add264e5e44c2940c089b39bdad0a3a20
aryal05 <aryal.rajat05@gmail.com>
2024-10-17 16:51:32 +0545

    decoration in navbar section

--- Shop/shopify/src/components/home/NavBar.jsx
+++ Shop/shopify/src/components/home/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
 
        <div className='flex justify-around items-center bg-gray-900 text-white h-10'>
         <ul className='flex space-x-10'>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Section</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact</a></li>   
+            <li className='hover:bg-green-400 hover:underline rounded-sm transition duration-200 ease-in-out'><a href="#">Home</a></li>
+            <li className='hover:bg-green-400 hover:underline rounded-sm transition duration-200 ease-in-out'><a href="#">Section</a></li>
+            <li className='hover:bg-green-400 hover:underline rounded-sm transition duration-200 ease-in-out'><a href="#">Blog</a></li>
+            <li className='hover:bg-green-400 hover:underline rounded-sm transition duration-200 ease-in-out'><a href="#">Contact</a></li>   
         </ul>
        </div>
       
