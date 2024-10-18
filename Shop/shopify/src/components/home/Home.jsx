5db0358e22109c422e80a5613e176c30093dc99d
aryal05 <aryal.rajat05@gmail.com>
2024-10-18 17:48:33 +0545

    home section layout

--- Shop/shopify/src/components/home/Home.jsx
+++ Shop/shopify/src/components/home/Home.jsx
@@ -3,17 +3,26 @@ import React from 'react'
 const Home = () => {
   return (
 
-    <div className='flex items-center justify-around'>
+    <div className='flex items-center justify-around '>
         <div>
             <h1>Top Rated
 
             </h1>
+            
+              <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
+            
         </div>
 
         <div>
             <h1>Items To Display
-
             </h1>
+            <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
         </div>
 
         <div>
@@ -21,6 +30,10 @@ const Home = () => {
                 
 
             </h1>
+            <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
+              <li>Movie 1</li>
         </div>
 
     </div>
