91eda302142727860833de852b4e0dd753d54ad1
aryal05 <aryal.rajat05@gmail.com>
2024-10-19 20:11:33 +0545

    finnal commit for today

--- Shop/shopify/src/components/Section.jsx
+++ Shop/shopify/src/components/Section.jsx
@@ -5,7 +5,7 @@ const Section = () => {
     <>
       <div className='border-black m-3 border-2 bg-amber-100 w-96 h-1/4 bg-opacity-15 flex justify-center rounded-2xl'>
         <div className=''>
-          <h1 className='px-4 py-2'>Service Section</h1>
+          <h1 className='underline decoration-slate-700 px-4 py-2'>Service Section</h1>
         </div>
       
       </div>
