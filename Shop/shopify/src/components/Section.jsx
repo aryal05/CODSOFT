01377630653b1f4e73896f909a939d1ecfaa45d6
aryal05 <aryal.rajat05@gmail.com>
2024-10-19 18:40:55 +0545

    section

--- Shop/shopify/src/components/Section.jsx
+++ Shop/shopify/src/components/Section.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const Section = () => {
   return (
     <>
-      <div className='border-black m-3 border-2 bg-amber-100 w-96 h-screen bg-opacity-15 flex justify-center rounded-2xl'>
+      <div className='border-black m-3 border-2 bg-amber-100 w-96 h-1/4 bg-opacity-15 flex justify-center rounded-2xl'>
         <div className=''>
           <h1 className='border border-green-300 px-4 py-2'>Service Section</h1>
         </div>
+      
+      </div>
+      <div className='border-black m-3 border-2 bg-amber-100 w-96 h-[500px] bg-opacity-15 flex rounded-2xl'>
+        <h1>hey</h1>
       </div>
     </>
   )
