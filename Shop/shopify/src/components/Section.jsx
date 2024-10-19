2f6c313a3793591f2ad139f144c2aeed1d51a6f3
aryal05 <aryal.rajat05@gmail.com>
2024-10-19 18:36:52 +0545

    bg color implementation

--- Shop/shopify/src/components/Section.jsx
+++ Shop/shopify/src/components/Section.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Section = () => {
   return (
     <>
-    <div>
-        <h1>Section</h1>
-    </div>
+      <div className='border-black m-3 border-2 bg-amber-100 w-96 h-screen bg-opacity-15 flex justify-center rounded-2xl'>
+        <div className=''>
+          <h1 className='border border-green-300 px-4 py-2'>Service Section</h1>
+        </div>
+      </div>
     </>
   )
 }
