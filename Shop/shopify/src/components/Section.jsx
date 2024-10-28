bf9d9e1e03a0cb6ddc0c66c75d1efae55780f8ac
aryal05 <aryal.rajat05@gmail.com>
2024-10-28 17:12:27 +0545

    section

--- Shop/shopify/src/components/Section.jsx
+++ Shop/shopify/src/components/Section.jsx
@@ -12,16 +12,13 @@ const Section = () => {
       <div className='border-black m-3 border-2 bg-amber-100 w-96 h-[500px] bg-opacity-15 flex rounded-2xl'>
         <h1 className='m-4'>Section Menu</h1>
         
-        <div className='flex items-center'> 
+        <div className='flex m-10 justify-center'> 
     <ul>
       
-    <li>
-            section list
-            section list
-            section list
-            section list
-            section list
-          </li>
+    <li> section list</li>
+    <li> section Items</li>
+    <li> section Decoration</li>
+    <li> section End</li>
     </ul>
         
         </div>
